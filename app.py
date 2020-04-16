6f9b6edafdb757b5849f86f7d4b656c5bf43488b
Jay Goel <jgoel@Jays-MacBook-Pro.local>
2020-04-16 13:18:13 -0400

    add code

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request, jsonify, redirect, make_response, url_for, flash
+from recipe_scrapers import scrape_me
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/recipe')
+def recipe():
+    url = request.args['url']
+
+    scraper = scrape_me(url)
+    recipe = {
+        'name': scraper.title(),
+        'ingredients': scraper.ingredients(),
+        'instructions': scraper.instructions().split("\n"),
+        'image': scraper.image(),
+        'url': url,
+    }
+
+    return render_template('recipe.html', recipe=recipe)
+
+if __name__ == '__main__':
+    app.run('localhost', 8080, debug=True, threaded=True)
