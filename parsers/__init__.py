a925fdc09e729a98e45d1a89e99c64b1714fe327
Michael Fivis <michael.fivis@gmail.com>
2020-06-27 01:18:56 +0200

    Ignore www subdomains
    If checking our own stash of parsers, strip www's from the parsed url
    netloc.

--- parsers/__init__.py
+++ parsers/__init__.py
@@ -6,12 +6,12 @@ from parsers.minimalistbaker import Minimalistbaker
 from parsers.bowlofdelicious import Bowlofdelicious
 
 PARSERS = {
-    'www.gimmesomeoven.com': Gimmesomeoven,
+    'gimmesomeoven.com': Gimmesomeoven,
     'smittenkitchen.com': Smittenkitchen,
     'letsdishrecipes.com': Letsdishrecipes,
     'lovingitvegan.com': Lovingitvegan,
     'minimalistbaker.com': Minimalistbaker,
-    'www.bowlofdelicious.com': Bowlofdelicious,
+    'bowlofdelicious.com': Bowlofdelicious,
 }
 
 def getParser(domain):
