78e99c560fe6cd9879d1e61fd32deb75b02255fe
Michael Fivis <michael.fivis@gmail.com>
2020-06-26 10:46:04 +0200

    Add bowlofdelicious parser

--- parsers/__init__.py
+++ parsers/__init__.py
@@ -3,6 +3,7 @@ from parsers.smittenkitchen import Smittenkitchen
 from parsers.letsdishrecipes import Letsdishrecipes
 from parsers.lovingitvegan import Lovingitvegan
 from parsers.minimalistbaker import Minimalistbaker
+from parsers.bowlofdelicious import Bowlofdelicious
 
 PARSERS = {
     'www.gimmesomeoven.com': Gimmesomeoven,
@@ -10,6 +11,7 @@ PARSERS = {
     'letsdishrecipes.com': Letsdishrecipes,
     'lovingitvegan.com': Lovingitvegan,
     'minimalistbaker.com': Minimalistbaker,
+    'www.bowlofdelicious.com': Bowlofdelicious,
 }
 
 def getParser(domain):
