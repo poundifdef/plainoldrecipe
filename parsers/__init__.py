90dcbe46a5aad8343ccb1065ad7f8c903e64caaa
Brendan Carpio <bfcarpio.git@mailnull.com>
2020-07-12 09:54:40 -0700

    Remove imports

--- parsers/__init__.py
+++ parsers/__init__.py
@@ -1,34 +1,23 @@
-from parsers.gimmesomeoven import Gimmesomeoven
 from parsers.smittenkitchen import Smittenkitchen
 from parsers.letsdishrecipes import Letsdishrecipes
-from parsers.lovingitvegan import Lovingitvegan
-from parsers.minimalistbaker import Minimalistbaker
-from parsers.bowlofdelicious import Bowlofdelicious
 from parsers.chefkoch import Chefkoch
 from parsers.hostthetoast import Hostthetoast
 from parsers.lecker import Lecker
 from parsers.essenundtriken import EssenUndTrinken
 from parsers.kuechengoetter import Kuechengoetter
-from parsers.kochbar import Kochbar
 from parsers.hostthetoast import Hostthetoast
-from parsers.thewoksoflife import Thewoksoflife
 
 # Must exclude the "www" portion of the URL
 PARSERS = {
-    'gimmesomeoven.com': Gimmesomeoven,
-    'smittenkitchen.com': Smittenkitchen,
-    'letsdishrecipes.com': Letsdishrecipes,
-    'lovingitvegan.com': Lovingitvegan,
-    'minimalistbaker.com': Minimalistbaker,
-    'chefkoch.de': Chefkoch,
-    'bowlofdelicious.com': Bowlofdelicious,
-    'hostthetoast.com': Hostthetoast,
-    'lecker.de': Lecker,
-    'essen-und-trinken.de': EssenUndTrinken,
-    'kuechengoetter.de' : Kuechengoetter,
-    'kochbar.de' : Kochbar,
-    'thewoksoflife.com': Thewoksoflife,
+    "smittenkitchen.com": Smittenkitchen,
+    "letsdishrecipes.com": Letsdishrecipes,
+    "chefkoch.de": Chefkoch,
+    "hostthetoast.com": Hostthetoast,
+    "lecker.de": Lecker,
+    "essen-und-trinken.de": EssenUndTrinken,
+    "kuechengoetter.de": Kuechengoetter,
 }
+
 
 def getParser(domain):
     parser = PARSERS.get(domain)
