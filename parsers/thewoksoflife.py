ceaa56c95b12549bf3ff8cfab112ffa0f15edcaa
jerjou <jerjou@users.noreply.github.com>
2020-07-17 14:44:23 -0400

    Remove double-parentheses from thewoksoflife

--- parsers/thewoksoflife.py
+++ parsers/thewoksoflife.py
@@ -3,4 +3,11 @@ import json
 from parsers.recipe import WpJsonRecipe
 
 class Thewoksoflife(WpJsonRecipe):
-    pass
+    def get_json_recipe(self, d):
+        recipe = super().get_json_recipe(d)
+        # thewoksoflife.com for some reason has double parentheses in its
+        # ingredients. Remove them.
+        recipe['ingredients'] = [
+                ingredient.replace('((', '(').replace('))', ')')
+                for ingredient in recipe.get('ingredients', [])]
+        return recipe
