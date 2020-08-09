7405ba57f90ec4401bedfa63268097d0f379ef0e
jgoel <jgoel@jgoels-MacBook-Pro.local>
2020-08-09 09:14:14 -0400

    Fix double-parentheses

--- parsers/minimalistbaker.py
+++ parsers/minimalistbaker.py
@@ -1,4 +1,12 @@
 from parsers.recipe import WpJsonRecipe
 
 class Minimalistbaker(WpJsonRecipe):
-    pass
+
+    def get_json_recipe(self, d):
+        recipe = super().get_json_recipe(d)
+        # thewoksoflife.com for some reason has double parentheses in its
+        # ingredients. Remove them.
+        recipe['ingredients'] = [
+                ingredient.replace('((', '(').replace('))', ')').replace('//', '/')
+                for ingredient in recipe.get('ingredients', [])]
+        return recipe
